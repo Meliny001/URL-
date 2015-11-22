85808f470813fd3c9719416da6e57c399d4ce38a
Zhuge_Mac <Zhuge_Mac@macdeiMac.local>
2015-11-22 13:36:07 +0800

    Initial Commit

---
+++ URL测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  URL测试
+//
+//  Created by Zhuge_Mac on 15/11/22.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ URL测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  URL测试
+//
+//  Created by Zhuge_Mac on 15/11/22.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
